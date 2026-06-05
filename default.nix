17f08870d7cbd54011ffe7d8d9981429d71bb0ef
cole-bassed <75517056+cole-bassed@users.noreply.github.com>
2026-06-05 02:18:42 -0500

    nixConf not nesting

--- default.nix
+++ default.nix
@@ -1,74 +1,63 @@
 {flake ? {}, ...}: let
-  src = rec {
-    names = {
-      src = "dots";
-      top = "_";
-      lib = "lix";
-    };
-
-    paths = {
-      src = ./.;
-      api = ./configurations/api;
-      dbg = ./debug;
-      documentation = ./documentation;
-      configurations = ./configurations;
-      templates = ./templates;
-      devShells = ./packages;
-      utilities = ./utilities;
-      secrets = ./secrets;
-      libraries = ./libraries;
-    };
-
-    defaults = {
-      host = {
-        name = "nixos";
-        id = null;
-        description = null;
-        type = null;
-        class = "nixos";
-        system = "x86_64-linux";
-        stateVersion = null; # ? Must be the same as when the OS was installed
-        paths.src = "/etc/nixos";
-
-        localization = {
-          latitude = 18.015;
-          longitude = -77.49;
-          locator = "manual";
-          city = "Mandeville/Jamaica";
-          timezone = "America/Jamaica";
-          locale = "en_US.UTF-8";
-        };
-      };
-
-      excludes = [
-        "archive"
-        "backup"
-        "review"
-        "temp"
-      ];
-
-      tags = [
-        "core"
-        "home"
-      ];
-    };
-
-    libraries = import paths.libraries {
-      inherit defaults flake paths names;
-    };
+  names = {
+    src = "dots";
+    top = "_";
+    lib = "lix";
   };
 
-  flake' =
-    if flake == {}
-    then {}
-    else flake;
-in
-  src
-  // {
-    flake =
-      if flake != {}
-      then flake
-      else null;
+  paths = {
+    src = ./.;
+    api = ./configurations/api;
+    dbg = ./debug;
+    documentation = ./documentation;
+    configurations = ./configurations;
+    templates = ./templates;
+    devShells = ./packages;
+    utilities = ./utilities;
+    secrets = ./secrets;
+    libraries = ./libraries;
+  };
 
-    "${src.names.lib}" = src.libraries;
+  defaults = {
+    host = {
+      name = "nixos";
+      id = null;
+      description = null;
+      type = null;
+      class = "nixos";
+      system = "x86_64-linux";
+      stateVersion = null; # ? Must be the same as when the OS was installed
+      paths.src = "/etc/nixos";
+
+      localization = {
+        latitude = 18.015;
+        longitude = -77.49;
+        locator = "manual";
+        city = "Mandeville/Jamaica";
+        timezone = "America/Jamaica";
+        locale = "en_US.UTF-8";
+      };
+    };
+
+    excludes = [
+      "archive"
+      "backup"
+      "review"
+      "temp"
+    ];
+
+    tags = [
+      "core"
+      "home"
+    ];
+  };
+
+  libraries = import paths.libraries {
+    inherit defaults flake paths names;
+  };
+in
+  libraries.optionalAttrs (flake != {}) {inherit flake;}
+  // {
+    inherit defaults libraries names paths;
+    "${names.lib}" = libraries;
   }
